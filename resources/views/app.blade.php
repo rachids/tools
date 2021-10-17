cd3a1e2c4bce04422edb7f74ff3ae026bcd66043
rachid <rachid@folkshr.com>
2021-10-17 23:57:21 +0200

    Ajout de vue et premier component

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tools</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="h-screen bg-gray-700">
+
+<div id="app" class="flex justify-center items-center h-screen">
+    <example-component></example-component>
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
